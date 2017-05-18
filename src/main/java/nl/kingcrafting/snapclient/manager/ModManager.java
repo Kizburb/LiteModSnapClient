dce200d41d45b8923bb2aa0602f00bfac58ecc54
kingcraftingnl <jaspersmit2801@gmail.com>
2017-05-18 22:02:32 +0200

    Working on new KeyBind system

--- src/main/java/nl/kingcrafting/snapclient/manager/ModManager.java
+++ src/main/java/nl/kingcrafting/snapclient/manager/ModManager.java
@@ -3,6 +3,7 @@ package nl.kingcrafting.snapclient.manager;
 import com.darkmagician6.eventapi.EventManager;
 import com.darkmagician6.eventapi.EventTarget;
 import com.mumfrey.liteloader.core.LiteLoader;
+import net.minecraft.client.settings.KeyBinding;
 import nl.kingcrafting.snapclient.events.EventTick;
 import nl.kingcrafting.snapclient.mod.BaseMod;
 import nl.kingcrafting.snapclient.mod.mods.ModFlight;
@@ -11,6 +12,7 @@ import nl.kingcrafting.snapclient.mod.mods.ModNoFall;
 import nl.kingcrafting.snapclient.mod.mods.ModStep;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Created by Jasper on 16-5-2017.
@@ -18,11 +20,15 @@ import java.util.ArrayList;
 public class ModManager {
 
     private static ModManager instance = new ModManager();
+
     public static ModManager getInstance(){
         return instance;
     }
 
     public ArrayList<BaseMod> mods = new ArrayList<BaseMod>();
+
+    public HashMap<BaseMod , KeyBinding> binds = new HashMap<BaseMod, KeyBinding>();
+
 
 
     public void initModManager(){
@@ -42,6 +48,11 @@ public class ModManager {
 
         for(BaseMod mod : ModManager.getInstance().mods){
             LiteLoader.getInput().registerKeyBinding(mod.getBind());
+        }
+        for(BaseMod mod : ModManager.getInstance().mods){
+            if(!binds.containsKey(mod)){
+                binds.put(mod , mod.getBind());
+            }
         }
 
     }
